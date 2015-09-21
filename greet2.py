e8e52a4eaca2d6e59f55860264d9501a15070972
关心 <jack.zhao829@gmail.com>
2015-09-21 21:37:35 +0800

    add python code

---
+++ greet2.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+name = 'Peter'
+
+name = 'John'
+
+def greet():
+	print "greet hello", name
+
+def greet_again():
+	name = 'Sue'
+	print "greet_name hello", name
+
+greet()
+print 'Hello' , name
