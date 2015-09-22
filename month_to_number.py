18f5ed3b295e1f0367184d17794b479c25ccf568
关心 <jack.zhao829@gmail.com>
2015-09-22 18:56:11 +0800

    add python code

---
+++ month_to_number.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+
+month = raw_input('What month is it?')
+
+if month == 'January':
+	print 1
+elif month == 'February':
+	print 2
+elif month == 'march':
+	print 3
+else:
+	print "later than March"
