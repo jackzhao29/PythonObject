b28f06f96ef9f01fb871a8a05fd69fa97515f6a8
关心 <jack.zhao829@gmail.com>
2015-09-23 19:43:39 +0800

    python code

---
+++ while.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+
+max_num = int(raw_input("Enter a number:"))
+i = 1
+
+while i <= max_num:
+	print i
+	i += 1
+print "done"
---
+++ while_list.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+
+mylist = [1, 2, 3, 4, 5, 6]
+length = len(mylist)
+
+i = 0
+#使用while循环list集合
+while i < length:
+	print mylist[i]
+	i += 1
+
+
+#使用for循环list集合
+
+for n in mylist:
+	print n
+
+
+
