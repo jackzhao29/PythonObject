1615840f158ec4d1f23905420f9dc8046a3934f5
关心 <jack.zhao829@gmail.com>
2015-11-02 21:09:01 +0800

    add python code

---
+++ ex33-while.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+i = 0
+numbers = []
+
+while  i < 6:
+	print "At the top i is %d" % i
+	numbers.append(i)
+
+	i = i + 1
+	print "Numbers now:", numbers
+	print "At the bottom i is %d" % i
+
+print numbers
+
+for i in numbers:
+	print i
