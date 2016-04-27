e05a7c2a201324d3ca4402e922e74730efafed0a
jackzhao29 <jack.zhao829@gmail.com>
2016-04-27 09:26:12 +0800

    add code

---
+++ ex_module_calss.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+import ex_module
+
+#use module example
+ex_module.apple()
+
+#use variable example
+print ex_module.tangering
+
+
+#use class example
+happy_boday =  MyStuff(["Happy birthady to you",
+	                   "I don't want to get sued",
+	                   "So I'll stop right there"])
+
+
+happy_boday.sing_me_song()
+
