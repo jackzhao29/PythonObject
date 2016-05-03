d63867103398cc03ce58e09eeca8c28916026fe9
jackzhao29 <jack.zhao829@gmail.com>
2016-05-03 23:12:09 +0800

    add python

---
+++ game_class_ex1.py
@@ -0,0 +1,58 @@
+#coding:utf-8
+#来自Percal 25号行星的哥顿人
+
+#场景类
+class Scene(object):
+
+	def enter(self):
+		pass
+
+#引擎类
+class Engine(object):
+
+	def __init__(self,scene_map):
+		pass
+
+	def play(self):
+		pass
+
+class Death(Scene):
+
+	def enter(self):
+		pass
+
+class Centralcorridor(Scene):
+
+	def enter(self):
+		pass
+
+class LaserWeaponArmory(Scene):
+
+	def enter(self):
+		pass
+
+class TheBridge(Scene):
+
+	def enter(self):
+		pass
+
+class EscapePod(Scene):
+
+	def enter(self):
+		pass
+
+class Map(object):
+
+	def __init__(self, start_scene):
+		pass
+
+	def next_scene(self, scene_name):
+		pass
+
+	def opening_scene(self):
+		pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
